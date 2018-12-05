5d42fc795b1e790602d2e8568cf125cff14f9c06
Daniel Flanagan <daniel@lytedev.io>
2018-12-04 22:45:42 -0600

    At least let it compile

--- lib/three.ex
+++ lib/three.ex
@@ -1,5 +1,6 @@
 defmodule Three do
   @dimension_parser ~r/\#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)/
+
   def part1(input_stream) do
     input_stream
     |> Stream.map(&parse_dimensions/1)
@@ -10,6 +11,10 @@ defmodule Three do
     l = Regex.run(@dimension_parser, str)
     [x, y, w, h] = l |> tl() |> Enum.map(&String.to_integer/1)
     {x, y, w, h}
+  end
+
+  def play_battleship(pos, {hit_once, flagged}) do
+    # MapSet.put(hit_
   end
 
   def part2(input_stream) do
